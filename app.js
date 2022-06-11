0a09d967bb59e36e001cdce7313fd1b17afa8aa1
valentineG <valegjohn@gmail.com>
2022-06-11 15:01:43 +0300

    make it better

--- app.js
+++ app.js
@@ -81,6 +81,6 @@ app.post('/sendmessage', async (req, res, next) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
