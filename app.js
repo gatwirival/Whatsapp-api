53f834ec1c1f9654be42c34ee6d713ec980d672c
valentineG <valegjohn@gmail.com>
2022-06-18 16:01:03 +0300

    make it better

--- app.js
+++ app.js
@@ -78,7 +78,21 @@ app.post('/sendmessage', async (req, res, next) => {
   }
 });
 
+const puppeteer = require("puppeteer");
 
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox"]
+  });
+  const tab = await browser.newPage();
+  const text = await (await tab.goto("http://example.com/")).text();
+  console.log(text);
+  console.log("done");
+  browser.close();
+}
+
+main();
 const PORT = process.env.PORT || 3000
 
 app.listen(process.env.PORT || 3000, function(){
