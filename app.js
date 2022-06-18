7a5653d72f69190ae29cf60290dd1cc0972d263d
valentineG <valegjohn@gmail.com>
2022-06-18 16:11:02 +0300

    Fixing deployment issue

--- app.js
+++ app.js
@@ -78,21 +78,13 @@ app.post('/sendmessage', async (req, res, next) => {
   }
 });
 
-const puppeteer = require("puppeteer");
+(async () => {
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.goto('https://fast.com');
+  await init(browser, page, observer, options);
+})().catch(observer.error.bind(observer));
 
-async function main() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox"]
-  });
-  const tab = await browser.newPage();
-  const text = await (await tab.goto("http://example.com/")).text();
-  console.log(text);
-  console.log("done");
-  browser.close();
-}
-
-main();
 const PORT = process.env.PORT || 3000
 
 app.listen(process.env.PORT || 3000, function(){
